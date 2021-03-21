c0d31ee6b48596743bde608382b3c874f0d32937
Megan <megan.c.yip@gmail.com>
2021-03-20 17:52:38 -0700

    Open source links

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <Main />
+    <p>
+      <a
+        href="https://github.com/MeganYippee/react-weather-app"
+        target="_blank"
+      >
+        Open source code
+      </a>
+      , by{" "}
+      <a href="https://github.com/MeganYippee" target="_blank">
+        Megan Yip
+      </a>
+    </p>
   </StrictMode>,
   rootElement
 );
