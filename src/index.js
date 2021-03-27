6ddc5dc3bd49569a0e7f2e2f196982c2a89d6c9f
Megan <megan.c.yip@gmail.com>
2021-03-26 20:24:02 -0700

    Cleaning up

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ ReactDOM.render(
     <p>
       <a
         href="https://github.com/MeganYippee/react-weather-app"
+        rel="noreferrer"
         target="_blank"
       >
         Open source code
       </a>
       , by{" "}
-      <a href="https://github.com/MeganYippee" target="_blank">
+      <a href="https://github.com/MeganYippee" rel="noreferrer" target="_blank">
         Megan Yip
       </a>
     </p>
