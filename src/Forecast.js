9903c8336e6970bd4579d0c507557418377ab6b4
Megan <megan.c.yip@gmail.com>
2021-04-03 15:50:13 -0700

    Connecting to axios and preparing for loop

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,9 +1,20 @@
 import "./Forecast.css";
 import ReactAnimatedWeather from "react-animated-weather";
+import axios from "axios";
 
 //<p className="next">In the next few days...</p>
 
 export default function Forecast() {
+  function handleForecast(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "2e24fad2691944b008429f223f37670a";
+  let lon = 40.7;
+  let lat = 74;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  axios.get(apiUrl).then(handleForecast);
+
   return (
     <div className="container bottom">
       <div className="row forecast">
@@ -19,48 +30,6 @@ export default function Forecast() {
             />{" "}
             <br />
             75° F
-          </h5>
-        </div>
-        <div className="col">
-          <h5>
-            Sun.
-            <br />
-            <ReactAnimatedWeather
-              icon="PARTLY_CLOUDY_DAY"
-              color="goldenrod"
-              size={30}
-              animate={true}
-            />{" "}
-            <br />
-            66 ° F{" "}
-          </h5>
-        </div>
-        <div className="col">
-          <h5>
-            Mon.
-            <br />
-            <ReactAnimatedWeather
-              icon="CLOUDY"
-              color="goldenrod"
-              size={30}
-              animate={true}
-            />{" "}
-            <br />
-            65° F{" "}
-          </h5>
-        </div>
-        <div className="col">
-          <h5>
-            Tues.
-            <br />
-            <ReactAnimatedWeather
-              icon="RAIN"
-              color="goldenrod"
-              size={30}
-              animate={true}
-            />{" "}
-            <br />
-            60° F{" "}
           </h5>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,21 +7,27 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <Main defaultCity="San Diego" />
-    <div className="footer">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.meganyipart.com/" rel="noreferrer" target="_blank">
-          Megan Yip
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/MeganYippee/react-weather-app"
-          rel="noreferrer"
-          target="_blank"
-        >
-          open source code.
-        </a>
-      </footer>
+    <div className="card">
+      <div className="footer">
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.meganyipart.com/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Megan Yip
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/MeganYippee/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open source code.
+          </a>
+        </footer>
+      </div>
     </div>
   </StrictMode>,
   rootElement
