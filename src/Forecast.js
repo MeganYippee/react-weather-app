43b46f75d4645bdc364b7d3368798d0be2d46824
Megan <megan.c.yip@gmail.com>
2021-04-04 19:51:09 -0700

    Fixed forecast city refresh

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
@@ -8,6 +8,11 @@ import ForecastDay from "./ForecastDay";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coord]);
+
   function handleForecast(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -24,6 +29,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
